b4ca6ba800aa7226cc5e4aa9fa59db051ec666e0
Mean <390602272@qq.com>
2021-10-10 22:41:05 +0800

    修复需要导入两次的bug

--- scheduleHtmlProvider.js
+++ scheduleHtmlProvider.js
@@ -2,23 +2,27 @@ function scheduleHtmlProvider(iframeContent = "", frameContent = "", dom = docum
     //除函数名外都可编辑
     //以下为示例，您可以完全重写或在此基础上更改
 
-    const ifrs = dom.getElementsByTagName("iframe");
-    const frs = dom.getElementsByTagName("frame");
+    //     const ifrs = dom.getElementsByTagName("iframe");
+    //     const frs = dom.getElementsByTagName("frame");
 
-    if (ifrs.length) {
-        for (let i = 0; i < ifrs.length; i++) {
-            const dom = ifrs[i].contentWindow.document;
-            iframeContent += scheduleHtmlProvider(iframeContent, frameContent, dom);
-        }
-    }
-    if (frs.length) {
-        for (let i = 0; i < frs.length; i++) {
-            const dom = frs[i].contentDocument.body.parentElement;
-            frameContent += scheduleHtmlProvider(iframeContent, frameContent, dom);
-        }
-    }
-    if (!ifrs.length && !frs.length) {
-        return dom.querySelector('body').outerHTML
-    }
-    return dom.getElementsByTagName('html')[0].innerHTML + iframeContent + frameContent
+    //     if (ifrs.length) {
+    //         for (let i = 0; i < ifrs.length; i++) {
+    //             const dom = ifrs[i].contentWindow.document;
+    //             iframeContent += scheduleHtmlProvider(iframeContent, frameContent, dom);
+    //         }
+    //     }
+    //     if (frs.length) {
+    //         for (let i = 0; i < frs.length; i++) {
+    //             const dom = frs[i].contentDocument.body.parentElement;
+    //             frameContent += scheduleHtmlProvider(iframeContent, frameContent, dom);
+    //         }
+    //     }
+    //     if (!ifrs.length && !frs.length) {
+    //         return dom.querySelector('body').outerHTML
+    //     }
+    //         return dom.getElementsByTagName('html')[0].innerHTML + iframeContent + frameContent
+    let iframe=dom.getElementsByTagName('iframe')[0].contentDocument
+    let table = iframe.getElementsByClassName('addlist_01')[0].outerHTML
+    console.info(table)
+    return table
 }
